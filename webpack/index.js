667ce2c627d645494cf33a87d6fc3c20e5790abb
ibaryshnikov <baryshnikov.il@gmail.com>
2019-04-30 08:23:11 +0300

    webpack example updated

--- webpack/index.js
+++ webpack/index.js
@@ -1,17 +1,15 @@
 import('./pkg') // webpack
-    .then(wasm => {
-        const array = [2, 5];
-
-        document
-            .body
-            .innerHTML += array.join(', ');
-
-        const newArray = wasm.updateArray(array);
-
-        document
-            .body
-            .innerHTML += `<br>${newArray.join(', ')}`
-
-        wasm.insertDiv();
-    })
+    .then(runExample)
     .catch(console.error);
+
+function runExample(wasm) {
+    const array = [2, 5];
+
+    document.body.innerHTML += array.join(', ') + '<br>';
+
+    const newArray = wasm.updateArray(array);
+
+    document.body.innerHTML += newArray.join(', ') + '<br>';
+
+    wasm.insertDiv();
+}
